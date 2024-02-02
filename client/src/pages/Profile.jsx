010679154d20387aed80e00d7218db6d3333b641
Ujjwal Suman <ujjwalsuman007@gmail.com>
2024-02-02 23:44:19 +0530

    complete update user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -7,26 +7,23 @@ import {
   uploadBytesResumable,
 } from 'firebase/storage';
 import { app } from '../../firebase';
-// import {
-//   updateUserStart,
-//   updateUserSuccess,
-//   updateUserFailure,
-//   deleteUserFailure,
-//   deleteUserStart,
-//   deleteUserSuccess,
-//   signOutUserStart,
-// } from '../redux/user/userSlice';
+import {
+  updateUserStart,
+  updateUserSuccess,
+  updateUserFailure,
+} from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
 
 export const Profile = () => {
 
   const fileRef = useRef(null);
-  const { currentUser } = useSelector((state) => state.user);
+  const { currentUser, loading, error } = useSelector((state) => state.user);
   const [file, setFile] = useState(undefined);
   const [filePerc, setFilePerc] = useState(0);
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
+  const dispatch = useDispatch();
+  const [updateSuccess, setUpdateSuccess] = useState(false);
   
   useEffect(() => {
     if (file) {
@@ -57,10 +54,43 @@ export const Profile = () => {
       }
     );
   };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault(); //preventing from refreshing the page 
+    try {
+      dispatch(updateUserStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+          dispatch(updateUserFailure(data.message));
+        return;
+      }
+
+      dispatch(updateUserSuccess(data));
+      setUpdateSuccess(true);
+
+      setTimeout(() => {
+        setUpdateSuccess(false);
+      }, 5000);
+    } catch (error) {
+        dispatch(updateUserFailure(data.message));
+    }
+  };
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
-      <form className='flex flex-col gap-4'>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input 
           onChange={(e) => setFile(e.target.files[0])}
           type='file' 
@@ -90,32 +120,31 @@ export const Profile = () => {
           <input
           type='text'
           placeholder='username'
-          // defaultValue={currentUser.username}
+          defaultValue={currentUser.username}
           id='username'
           className='border p-3 rounded-lg'
-          // onChange={handleChange}
+          onChange={handleChange}
         />
         <input
           type='email'
           placeholder='email'
           id='email'
-          // defaultValue={currentUser.email}
+          defaultValue={currentUser.email}
           className='border p-3 rounded-lg'
-          // onChange={handleChange}
+          onChange={handleChange}
         />
         <input
           type='password'
           placeholder='password'
-          // onChange={handleChange}
+          onChange={handleChange}
           id='password'
           className='border p-3 rounded-lg'
         />
         <button
-          // disabled={loading}
+          disabled={loading}
           className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'
         >
-          {/* {loading ? 'Loading...' : 'Update'} */}
-          Update
+          {loading ? 'Loading...' : 'Update'}
         </button>
 
       </form>
@@ -132,6 +161,10 @@ export const Profile = () => {
           Sign out
         </span>
       </div>
+      <p className='text-red-700 mt-5'>{error ? error : ''}</p>
+      <p className='text-green-700 mt-5'>
+        {updateSuccess ? 'User is updated successfully!' : ''}
+      </p>
     </div>
   )
 }
