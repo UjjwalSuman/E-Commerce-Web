b891e06fc75e1d7440bded46b7eec87792c04b72
Ujjwal Suman <ujjwalsuman007@gmail.com>
2024-02-14 22:56:13 +0530

    complete home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Home } from './pages/Home'
 import { About } from './pages/About'
 import { Profile } from './pages/Profile'
 import { SignIn } from './pages/SignIn'
@@ -11,6 +10,7 @@ import CreateListing from './pages/CreateListing'
 import UpdateListing from './pages/UpdateListing';
 import Listing from './pages/Listing';
 import Search from './pages/Search';
+import Home from './pages/Home'
 
 
 
